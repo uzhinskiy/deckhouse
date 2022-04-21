74e46ba3d803ce2fe1248966110e4a3ada9f7c9c
Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>
2022-04-21 19:16:03 +0300

    [prometheus] Add app-root annotation for example `Connecting Prometheus to an external Grafana instance`

--- modules/040-control-plane-manager/hooks/audit_policy_basic_targets_generated.go
+++ modules/040-control-plane-manager/hooks/audit_policy_basic_targets_generated.go
@@ -44,7 +44,7 @@ var auditPolicyBasicServiceAccounts = []string{
 	"cert-exporter",
 	"cert-manager",
 	"cloud-controller-manager",
-	"cloud-provider-yandex-cloud-metrics-exporter",
+	"cloud-metrics-exporter",
 	"cluster-autoscaler",
 	"cni-flannel",
 	"cni-simple-bridge",
